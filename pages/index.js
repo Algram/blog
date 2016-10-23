355b30ebf2a3bdc2931c2b121b4becd5519a2a03
Algram <aliasgram@gmail.com>
2016-10-23 12:31:15 +0200

    Added eslint and started cleaning up

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,54 @@
-import React from 'react'
-import { Link } from 'react-router'
-import sortBy from 'lodash/sortBy'
-import { prefixLink } from 'gatsby-helpers'
-import { rhythm } from 'utils/typography'
-import Helmet from "react-helmet"
-import access from 'safe-access'
-import { config } from 'config'
-import include from 'underscore.string/include'
-import Bio from 'components/Bio'
+import React from 'react';
+import { Link } from 'react-router';
+import sortBy from 'lodash/sortBy';
+import { prefixLink } from 'gatsby-helpers';
+import { rhythm } from 'utils/typography';
+import Helmet from 'react-helmet';
+import access from 'safe-access';
+import { config } from 'config';
+import include from 'underscore.string/include';
+import Bio from 'components/Bio';
 
 class BlogIndex extends React.Component {
-  render () {
-    const pageLinks = []
+  render() {
+    const pageLinks = [];
     // Sort pages.
-    const sortedPages = sortBy(this.props.route.pages, (page) =>
+    const sortedPages = sortBy(this.props.route.pages, page =>
       access(page, 'data.date')
-    ).reverse()
+    ).reverse();
     sortedPages.forEach((page) => {
       if (access(page, 'file.ext') === 'md' && !include(page.path, '/404')) {
-        const title = access(page, 'data.title') || page.path
+        const title = access(page, 'data.title') || page.path;
+
+        const body = access(page, 'data.body');
+        const bodyFirstPStart = body.indexOf('<p>') + 2;
+        const bodyFirstPEnd = body.indexOf('</p>') - bodyFirstPStart;
+        const description = body.substring(bodyFirstPStart, bodyFirstPEnd);
+
+        const datePublished = access(page, 'data.date');
+        const category = access(page, 'data.category');
+        console.log(page);
+
         pageLinks.push(
+          // Here goes the preview post
           <li
             key={page.path}
             style={{
-              marginBottom: rhythm(1/4),
+              marginBottom: rhythm(1 / 4)
             }}
           >
-            <Link style={{boxShadow: 'none'}} to={prefixLink(page.path)}>{title}</Link>
+            <Link style={{ boxShadow: 'none' }} to={prefixLink(page.path)}>{title}</Link>
           </li>
-        )
+        );
       }
-    })
+    });
     return (
       <div>
         <Helmet
           title={config.blogTitle}
           meta={[
-            {"name": "description", "content": "Sample blog"},
-            {"name": "keywords", "content": "blog, articles"},
+            { name: 'description', content: 'Sample blog' },
+            { name: 'keywords', content: 'blog, articles' }
           ]}
         />
         <Bio />
@@ -45,12 +56,12 @@ class BlogIndex extends React.Component {
           {pageLinks}
         </ul>
       </div>
-    )
+    );
   }
 }
 
 BlogIndex.propTypes = {
-  route: React.PropTypes.object,
-}
+  route: React.PropTypes.object
+};
 
-export default BlogIndex
+export default BlogIndex;
