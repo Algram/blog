42edd6773e7724fabfd6b6c11885be8494516924
Algram <aliasgram@gmail.com>
2016-10-26 11:13:19 +0200

    Fix for scrolling content on mobile

--- html.js
+++ html.js
@@ -23,11 +23,8 @@ module.exports = React.createClass({
       <html lang="en">
         <head>
           <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <meta http-equiv="x-ua-compatible" content="ie=edge" />
           {head.title.toComponent()}
           {head.meta.toComponent()}
           {css}
