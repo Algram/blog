e7a9c3a3164667ac6586a5c1ca51dc60bf4dd5bd
Algram <aliasgram@gmail.com>
2016-11-07 12:56:33 +0100

    autosave for piwik

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,14 @@
 /* global window */
+let firstPage = true;
 
 exports.onRouteUpdate = (state, page) => {
+  if (firstPage) {
+    firstPage = false;
+    return;
+  }
+
   window._paq = window._paq || [];
   window._paq.push(['setCustomUrl', state.pathname]);
   window._paq.push(['setDocumentTitle', page && page.data && page.data.title ? page.data.title : state.pathname]);
-  window._paq.push(['trackPageView']);
+  window._paq.push(['trackPageView', page && page.data && page.data.title ? page.data.title : state.pathname]);
 };
