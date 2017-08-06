383ae5842059dc6bbaede3b8fd3aaa3b83573d5e
Algram <aliasgram@gmail.com>
2017-08-06 13:41:44 +0200

    Added loading indicator

---
+++ src/components/Loader.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+import { colors } from '../util/style-utils'
+
+const rotate = keyframes`
+  0% {transform: rotate(0deg);}
+  25% {transform: rotate(180deg);}
+  50% {transform: rotate(180deg);}
+  75% {transform: rotate(360deg);}
+  100% {transform: rotate(360deg);}
+`
+
+const fill = keyframes`
+  0% {height: 0%;}
+  25% {height: 0%;}
+  50% {height: 100%;}
+  75% {height: 100%;}
+  100% {height: 0%;}
+`
+
+const LoaderWrapper = styled.div`
+  display:flex;
+  justify-content: center;
+  margin-top: 20px;
+  margin-bottom: 100px;
+`
+
+const LoaderText = styled.span`
+  font-family: 'Roboto Mono';
+  font-weight: bold;
+  font-size: 1.2em;
+  color: ${colors.colorBackgroundDark};
+  margin: 0 10px 0 -10px;
+`
+
+const Loader = styled.span`
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border: 4px solid ${colors.colorBackgroundDark};
+  animation: ${rotate} 3s infinite ease;
+`
+
+const LoaderInner = styled.span`
+  vertical-align: top;
+  display: inline-block;
+  width: 100%;
+  background-color: ${colors.colorBackgroundDark};
+  animation: ${fill} 3s infinite ease-in;
+`
+
+export default () => {
+  return (
+    <LoaderWrapper>
+      <LoaderText>Loading</LoaderText>
+      <Loader>
+        <LoaderInner />
+      </Loader>
+    </LoaderWrapper>
+  )
+}
--- src/components/comments/CommentsList.js
+++ src/components/comments/CommentsList.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react'
-import Comment from './Comment'
+import styled from 'styled-components'
+import { darken } from 'polished'
+import { colors } from '../../util/style-utils'
 import { isScrolledIntoView } from '../../util/helpers'
+import Loader from '../Loader'
+import Comment from './Comment'
 import mock from './comments.json'
+
+const Header = styled.div`
+  display: flex;
+  margin-top: 30px;
+  margin-bottom: 10px;
+`
+
+const Button = styled.button`
+  flex-grow: 0;
+  height: 40px;
+  padding: 10px;
+  border: none;
+  background: ${colors.colorAccent};
+  color: ${colors.colorTextLight};
+
+  &:hover {
+    cursor: pointer;
+    background: ${darken(0.05, colors.colorAccent)};
+  }
+`
+
+const H2 = styled.h2`
+  margin-top: 0;
+  flex-grow: 1;
+`
 
 class CommentsList extends Component {
   constructor () {
@@ -18,12 +47,16 @@ class CommentsList extends Component {
       .then(response => response.json())
       .then(json => {
         console.log(json)
-        this.setState({ comments: json, loading: false })
+        this.setState({ comments: json.reverse(), loading: false })
       })
   }
 
   componentDidMount () {
-    const commentsList = document.querySelector('.comments.comments__list')
+    this.setState({ loading: true })
+    setTimeout(() => {
+      this.setState({ loading: false })
+    }, 2000)
+    const commentsList = document.querySelector('.js-comments')
 
     document.addEventListener('scroll', () => {
       if (isScrolledIntoView(commentsList) &&
@@ -38,13 +71,16 @@ class CommentsList extends Component {
   render () {
     if (this.state.loading) {
       return (
-        <div>LOADING</div>
+        <Loader />
       )
     }
 
     return (
-      <div className='comments comments__list'>
-        <button>Comment</button><br /><br />
+      <div className='js-comments'>
+        <Header>
+          <H2>Comments</H2>
+          <Button>Leave a Comment over at GitHub</Button>
+        </Header>
         <div>
           {this.state.comments.map(comment =>
             <Comment data={comment} />
